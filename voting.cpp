1ffc281d22b8072b398d3d19dfe054525383c3d9
Kostiantyn Kozko <kostiantyn.kozko@ringteam.com>
2024-06-10 21:45:56 +0300

    New format

--- voting.cpp
+++ voting.cpp
@@ -4,20 +4,22 @@
 #include <sstream>
 #include <map>
 namespace fs = std::filesystem;
+
 std::map<uint32_t, uint32_t> parse_voting(fs::path file)
 {
     std::fstream voting_file(file, std::fstream::in);
-    std::string token;
-    const char rows_delim = ' ';
+    std::string line;
     std::map<uint32_t, uint32_t> votes;
-    while(std::getline(voting_file, token, rows_delim))
+
+    while (std::getline(voting_file, line))
     {
-        if(token.find("SEL=") != std::string::npos)
+        std::string::size_type pos = line.find("V=");
+        if (pos != std::string::npos)
         {
-            std::string vote_str=token.substr(4);
+            std::string vote_str = line.substr(pos + 2);  // +2 to skip "V="
             std::stringstream vote_stream(vote_str);
             std::string vote_entry;
-            while(std::getline(vote_stream, vote_entry, ','))
+            while (std::getline(vote_stream, vote_entry, ','))
             {
                 ++votes[std::stoi(vote_entry)];
             }
@@ -26,13 +28,13 @@ std::map<uint32_t, uint32_t> parse_voting(fs::path file)
     return votes;
 }
 
-std::map <uint32_t, std::string> parse_names(fs::path file)
+std::map<uint32_t, std::string> parse_names(fs::path file)
 {
     std::fstream names_file(file, std::fstream::in);
     std::string line;
     std::map<uint32_t, std::string> names;
     uint32_t count = 0;
-    while(std::getline(names_file, line))
+    while (std::getline(names_file, line))
     {
         ++count;
         names[count] = line;
@@ -45,37 +47,38 @@ int generate_csv(const std::multimap<uint32_t, uint32_t, std::greater<uint32_t>>
     std::cout << "Name,Votes count" << std::endl;
     if (names.empty() || names.size() != votes.size())
     {
-        for (auto& it : votes)
+        for (const auto& it : votes)
         {
             std::cout << it.second << "," << it.first << std::endl;
         }
     }
     else
     {
-        for (auto& it : votes)
+        for (const auto& it : votes)
         {
             std::cout << names.at(it.second) << "," << it.first << std::endl;
-        } 
+        }
     }
 
     return 0;
 }
+
 int main(int argc, char *argv[])
 {
     if (argc < 2 || argc > 3)
     {
-       std::cout << "Usage: ./voting <voting_report> [<list of names>]" << std::endl;
-       return -1;
+        std::cout << "Usage: ./voting <voting_report> [<list of names>]" << std::endl;
+        return -1;
     }
 
     std::map<uint32_t, uint32_t> votes = parse_voting(argv[1]);
     if (votes.empty())
     {
-       std::cerr << "Wrong file with voting report" << std::endl;
-       return -2;
+        std::cerr << "Wrong file with voting report" << std::endl;
+        return -2;
     }
 
-    std::map <uint32_t, std::string> names_map;
+    std::map<uint32_t, std::string> names_map;
     if (argc == 3)
     {
         names_map = parse_names(argv[2]);
@@ -86,7 +89,7 @@ int main(int argc, char *argv[])
     }
 
     std::multimap<uint32_t, uint32_t, std::greater<uint32_t>> sorted_votes;
-    for (auto& it : votes)
+    for (const auto& it : votes)
     {
         sorted_votes.insert({it.second, it.first});
     }
@@ -95,3 +98,4 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
